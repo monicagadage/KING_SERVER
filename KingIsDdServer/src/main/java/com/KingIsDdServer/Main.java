1d52421ae9a27ecc7db2dbb8a6bc98019b8567fa
monikagadage <44680826+monikagadage@users.noreply.github.com>
2021-05-03 00:39:38 +0530

    Update Main.java
    second commit

--- KingIsDdServer/src/main/java/com/KingIsDdServer/Main.java
+++ KingIsDdServer/src/main/java/com/KingIsDdServer/Main.java
@@ -1,43 +1,90 @@
 package com.KingIsDdServer;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import static java.nio.file.StandardOpenOption.CREATE_NEW;
+public class GameProcessing {
 
-public class Main {
 	
-	public static Boolean ExitGame = false;
-	public static void main(String[] args) throws IOException, InterruptedException {
-	
-        for (int i = 1 ; i <=2 ;i ++) {
-        	String playerName = "P" + i;
-        	String readPath = "/tmp/From" + playerName;
-        	String writePath = "/tmp/To" + playerName;
-        	 File readFileName = new File(readPath);
-        	 readFileName.deleteOnExit();
-         File writeFileName = new File(writePath);
-         writeFileName.deleteOnExit();
-         writeFileName.setReadable(true);
-         writeFileName.setWritable(true);
-         readFileName.setReadable(true);
-         readFileName.setWritable(true);
-         readFileName.setExecutable(true);
-         writeFileName.setExecutable(true);
-        	OutputStream readFile = Files.newOutputStream(readFileName.toPath(), CREATE_NEW);
-       
+	public static void initializeFile(String filePath) throws InterruptedException {
 
-        	OutputStream writeFile = Files.newOutputStream(writeFileName.toPath(), CREATE_NEW);
-
-        		String input = "Hello " + playerName;
-        		writeFile.write(input.getBytes());
-        		writeFile.close();
-        }
-       
-        	System.out.println("in");
-        	Utility.readFile(ExitGame);      
-	}
+		Random random = new Random();
+		
+		StringBuilder message = new StringBuilder(Constant.MESSAAGE_02).append(":");
+		message.append(Constant.MORAY_CHAR).append(Constant.COMMA)
+				.append(Constant.FOUR).append(Constant.ZERO).append(Constant.ZERO)
+				.append(Constant.SEMICOLLEN).append(Constant.GWYNEDD_CHAR).append(Constant.COMMA)
+				.append(Constant.ZERO).append(Constant.FOUR).append(Constant.ZERO)
+				.append(Constant.SEMICOLLEN).append(Constant.ESSEX_CHAR).append(Constant.COMMA)
+				.append(Constant.ZERO).append(Constant.ZERO).append(Constant.FOUR);
+		
+		ArrayList<String> loca = new ArrayList<String>();
+		loca.add(Constant.STRATHCLYDE_CHAR);
+		loca.add(Constant.LANCASTER_CHAR);
+		loca.add(Constant.NORTHUMBRIA_CHAR);
+		loca.add(Constant.WARWICK_CHAR);
+		loca.add(Constant.DEVON_CHAR);
+		
+		for ( int i = 0 ; i < 5 ; i ++) {
+			
+			int n1 = random.nextInt(4);
+			int n2 = random.nextInt(4 - n1);
+			int n3 = 4 - n1 - n2;	
+			message.append(Constant.SEMICOLLEN).append(loca.get(i)).append(Constant.COMMA).append(n1).append(n2).append(n3);
+		}
+		Utility.writeAllFile(filePath, message.toString());
 
 }
+
+//03 message
+public static void distributeFoll(String filePath) throws InterruptedException {
+	Random random = new Random();
+
+	StringBuilder message = new StringBuilder(Constant.MESSAAGE_03).append(":");
+	for(int i = 1 ; i <= 3;  i ++ ) {
+		
+			int n1 = random.nextInt(3);
+			int n2 = random.nextInt(3 - n1);
+			int n3 = 3 - n1 - n2;		
+			String playerName = "P" + i;
+			if(i < 3)
+				message.append(playerName).append(Constant.COMMA).append(n1).append(n2).append(n3).append(Constant.COMMA);
+			else
+				message.append(playerName).append(Constant.COMMA).append(n1).append(n2).append(n3);
+	}
+	Utility.writeAllFile(filePath, message.toString());
+	
+}
+//04 message
+public static void distributeReg(String filePath) throws InterruptedException {
+	Random random = new Random();
+
+	StringBuilder message = new StringBuilder(Constant.MESSAAGE_04).append(":");
+	
+	ArrayList<String> loca = new ArrayList<String>();
+	loca.add(Constant.MORAY_CHAR);
+	loca.add(Constant.GWYNEDD_CHAR);
+	loca.add(Constant.ESSEX_CHAR);
+	loca.add(Constant.STRATHCLYDE_CHAR);
+	loca.add(Constant.LANCASTER_CHAR);
+	loca.add(Constant.NORTHUMBRIA_CHAR);
+	loca.add(Constant.WARWICK_CHAR);
+	loca.add(Constant.DEVON_CHAR);
+	
+	for(int i = 7 ; i >= 0;  i -- ) {
+		
+			if(i > 0) {
+				int n1 = random.nextInt(i);
+				message.append(loca.get(n1)).append(Constant.COMMA);
+				loca.remove(n1);
+			}
+			else
+				message.append(loca.get(0));
+			
+	}
+	Utility.writeAllFile(filePath, message.toString());
+	
+}
+
+}
+
