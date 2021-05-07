14a32d4342168e3f4f2ddf8cb102ab11194121e4
monikagadage <44680826+monikagadage@users.noreply.github.com>
2021-05-07 12:26:52 +0530

    Update Main.java
    fifo files

--- KingIsDdServer/src/main/java/com/KingIsDdServer/Main.java
+++ KingIsDdServer/src/main/java/com/KingIsDdServer/Main.java
@@ -10,35 +10,46 @@ import static java.nio.file.StandardOpenOption.CREATE_NEW;
 public class Main {
 	
 	public static Boolean ExitGame = false;
-	public static void main(String[] args) throws IOException, InterruptedException {
-		
-		String readFile = "/tmp/From";
-		String writeFile = "/tmp/To";
-	
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String readFile = "/tmp/From";
+        String writeFile = "/tmp/To";
+
         for (int i = 1 ; i <=3 ;i ++) {
-        	
-	        	String playerName = "P" + i;
-	        	String readPath = readFile + playerName;
-	        	String writePath = writeFile + playerName;
-	        	
-	        	 File readFileName = new File(readPath);
-	        	 readFileName.deleteOnExit();
-	        	 
-	         File writeFileName = new File(writePath);
-	         writeFileName.deleteOnExit();
-	         
-	         writeFileName.setReadable(true);
-	         writeFileName.setWritable(true);
-	         readFileName.setReadable(true);
-	         readFileName.setWritable(true);
-	         readFileName.setExecutable(true);
-	         writeFileName.setExecutable(true);
-	         
-	        Files.newOutputStream(readFileName.toPath(), CREATE_NEW);
-	      
-	        Files.newOutputStream(writeFileName.toPath(), CREATE_NEW);
+
+                String playerName = "P" + i;
+                String readPath = readFile + playerName;
+                String writePath = writeFile + playerName;
+
+                 File readFileName = new File(readPath);
+                 readFileName.deleteOnExit();
+
+             File writeFileName = new File(writePath);
+             writeFileName.deleteOnExit();
+
+             writeFileName.setReadable(true);
+             writeFileName.setWritable(true);
+             readFileName.setReadable(true);
+             readFileName.setWritable(true);
+             readFileName.setExecutable(true);
+             writeFileName.setExecutable(true);
+
+          //    Runtime.getRuntime().exec(new String[] { "cd /"});
+
+          //Files.newOutputStream(writeFileName.toPath());
+
+          //Files.newOutputStream(readFileName.toPath());
+ Runtime.getRuntime().exec(new String[] {"mkfifo","-m", "777",  readFileName.toPath().toString()});
+             Runtime.getRuntime().exec(new String[] {"mkfifo","-m","777",writeFileName.toPath().toString()});
+  // Runtime.getRuntime().exec(new String[] { "cd /"});
+
+             //Runtime.getRuntime().exec(new String[] { "chmod 775 " + readFileName.toPath().toString() });
+             //Runtime.getRuntime().exec(new String[] { "chmod 775 " + writeFileName.toPath().toString() });
+         // Files.newOutputStream(readFileName.toPath());
+
+          //Files.newOutputStream(writeFileName.toPath());
         }
-        
+
         
         		
         		System.out.println("fisrt");
